641fb630ba90f22a939d7e9645c57a663c2eafd9
Tikhon <tihonbikbaev@gmail.com>
2024-06-05 23:55:46 +0300

    11.10 great comeback

--- app/src/main/java/com/example/messenger2/UsersActivity.java
+++ app/src/main/java/com/example/messenger2/UsersActivity.java
@@ -10,11 +10,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class UsersActivity extends AppCompatActivity {
     private UserViewModel viewModel;
+    private RecyclerView recyclerViewUsers;
+    private UsersAdapter usersAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,12 @@ public class UsersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_users);
         viewModel = new ViewModelProvider(this).get(UserViewModel.class);
         observeViewModel();
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            User user = new User("id" + i, "name" + i, "lastName" + i, i, new Random().nextBoolean());
+            users.add(user);
+        }
+        usersAdapter.setUsers(users);
     }
 
     public static Intent newIntent(Context context) {
@@ -39,6 +52,12 @@ public class UsersActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void initViews() {
+        recyclerViewUsers = findViewById(R.id.recyclerViewUsers);
+        usersAdapter = new UsersAdapter();
+        recyclerViewUsers.setAdapter(usersAdapter);
     }
 
     @Override
