6869813768f437b2aa4d3073c136f99b1b554e85
Tikhon <tihonbikbaev@gmail.com>
2024-06-15 23:39:40 +0300

    11.14 not full

---
+++ app/src/main/java/com/example/messenger2/Message.java
@@ -0,0 +1,28 @@
+package com.example.messenger2;
+
+public class Message {
+    private String text;
+    private String senderId;
+    private String receiverId;
+
+    public Message(String text, String senderId, String receiverId) {
+        this.text = text;
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+    }
+
+    public Message() {
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public String getReceiverId() {
+        return receiverId;
+    }
+}
